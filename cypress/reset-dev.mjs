ad611cf06818a2228d5a32fc65aec9d6abc259e6
unlocomqx <amxmodx.unloco@gmail.com>
2021-11-06 01:28:56 +0100

    Change database name

--- cypress/reset-dev.mjs
+++ cypress/reset-dev.mjs
@@ -2,4 +2,4 @@
 
 await $`dotenv -e .env.test -- prisma migrate reset --force`;
 await $`dotenv -e .env.test -- prisma db push --accept-data-loss`;
-await $`psql --username postgres test_dynamicproduct < ./cypress/seeds/base-dev.sql`;
+await $`psql --username postgres test_my_database < ./cypress/seeds/base-dev.sql`;
